4d45320d07185e5e6cd58d92479d912cd9ad3f4f
Isabel Dec <idec@id.iit.edu>
2017-04-28 00:17:13 -0500

    squares visible in browser.

--- part-2/script.js
+++ part-2/script.js
@@ -1,2 +1,11 @@
 // Do your work in this file.
 
+
+var i = 0; 
+for (i = 0; i <= 196; i++) {
+  $("<div>").addClass("box type-1").appendTo(document.body);
+  "<br>";
+  $("<div>").addClass("box type-2").appendTo(document.body);
+  "<br>";
+} 
+
