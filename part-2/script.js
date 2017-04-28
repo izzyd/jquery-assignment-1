efb058efd89aeabfc5ef3f88905d9452da83c2c5
Isabel Dec <idec@id.iit.edu>
2017-04-28 00:36:10 -0500

    boxes clickable; i’m done (i think).

--- part-2/script.js
+++ part-2/script.js
@@ -1,11 +1,23 @@
 // Do your work in this file.
 
 
-var i = 0; 
+var i = 0;
 for (i = 0; i <= 196; i++) {
-  $("<div>").addClass("box type-1").appendTo(document.body);
-  "<br>";
-  $("<div>").addClass("box type-2").appendTo(document.body);
-  "<br>";
-} 
+    $("<div>").addClass("box type-1").appendTo(document.body);
+    "<br>";
+    $("<div>").addClass("box type-2").appendTo(document.body);
+    "<br>";
+}
 
+// function boxClicked()
+
+$(".box").click(function () {
+    if ($(this).hasClass("clicked")) {
+        $(this).removeClass("clicked");
+    }
+    else {
+        $(this).addClass("clicked");
+    }
+}); 
+
+// addEventListener 
