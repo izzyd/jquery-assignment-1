1e88715f583260979b82c17f589a72d524a46fc4
Isabel Dec <idec@id.iit.edu>
2017-04-27 21:26:54 -0500

    setting divs and adding classes.

--- part-1/script.js
+++ part-1/script.js
@@ -1,6 +1,21 @@
 // Do your work in this file.
 
-var view = $('.view');
-var grass = $('.grass');
-var sun = $('.sun');
-var 
+// var view = $('.view');
+// var grass = $('.grass');
+// var sun = $('.sun');
+
+// //.append() 
+// var $newdiv1 = $( "<div id='view'></div>" ),
+//   newdiv2 = document.createElement( "div" ),
+//   existingdiv1 = document.getElementById( "foo" );
+ 
+// $( "body" ).append( $newdiv1, [ newdiv2, existingdiv1 ] );
+
+// //.appendTo() 
+// $( "h2" ).appendTo( $( ".container" ) );
+// $( "view" ).appendTo( $( document.body ) );
+// $( "grass").appendTo( $( "view" ) ); 
+
+$("<div>").addClass("view").appendTo(document.body);
+$("<div>").addClass("grass").appendTo(".view");
+
